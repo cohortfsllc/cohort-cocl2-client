5dec5fc6b3dc934511332ec23eaa571abb10f4b2
Eric I <eric@linuxbox.com>
2015-05-29 10:49:45 -0400

    Have run script generate nacl-gdb.cmds so debugger can load source files.

--- imc-test/run.sh
+++ imc-test/run.sh
@@ -4,6 +4,13 @@ sel_ldr="${HOME}/CoCl2/native_client/native_client/scons-out/dbg-linux-x86-64/st
 irt="${HOME}/CoCl2/native_client/native_client/scons-out/nacl_irt-x86-64/staging/irt_core.nexe"
 nexe="imc_test_client_x86_64.nexe"
 
+
+cat >nacl-gdb.cmds <<EOF
+target remote localhost:4014
+nacl-irt `readlink -f $irt`
+file `readlink -f $nexe`
+EOF
+
 set -x
 # ${sel_ldr} -B ${irt} -- ${nexe}
 ./runner $sel_ldr $irt $nexe
